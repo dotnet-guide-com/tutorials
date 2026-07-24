cbb200e603da05054570cbd28384e1a29f0f0f87
balajibashyam <balajibashyam@gmail.com>
2026-07-24 20:57:49 +0530

    Delete aspnet-core directory

--- aspnet-core/TodoApiBasic/Todo.cs
+++
@@ -1,7 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-public record Todo(
-    [Range(1, int.MaxValue)] int Id,
-    [Required, MinLength(2)] string Title,
-    bool IsComplete = false
-);
